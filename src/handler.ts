704016f3fa0e9f76a3a0760d0bc02f79ed76d296
Akhil Narang <me@akhilnarang.dev>
2020-06-04 12:12:33 +0530

    redirekt: simplify a bit
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,6 @@
 const BASE_URL = 'https://aosip.dev/'
 const GITHUB_USERNAME = 'AOSiP'
+const GITHUB_URL = `https://github.com/${GITHUB_USERNAME}`
 
 export async function handleRequest(request: Request): Promise<Response> {
   const relURL = request.url.replace(BASE_URL, '')
@@ -7,15 +8,15 @@ export async function handleRequest(request: Request): Promise<Response> {
   if (URLparts[0] === 'g') {
     switch (URLparts.length) {
       case 1:
-        return Response.redirect(`https://github.com/${GITHUB_USERNAME}`, 301)
+        return Response.redirect(`${GITHUB_URL}`, 301)
       case 2:
         return Response.redirect(
-          `https://github.com/${GITHUB_USERNAME}/${URLparts[1]}`,
+          `${GITHUB_URL}/${URLparts[1]}`,
           301,
         )
       case 3:
         return Response.redirect(
-          `https://github.com/${GITHUB_USERNAME}/${URLparts[1]}/commit/${URLparts[2]}`,
+          `${GITHUB_URL}/${URLparts[1]}/commit/${URLparts[2]}`,
           301,
         )
       default:
