23776dfa9b60bceec5531c4ac14e42745c387710
Akhil Narang <me@akhilnarang.dev>
2020-06-04 12:17:08 +0530

    redirekt: add support for AOSiP-Devices github
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://aosip.dev/'
-const GITHUB_USERNAME = 'AOSiP'
-const GITHUB_URL = `https://github.com/${GITHUB_USERNAME}`
+const GITHUB_URL = `https://github.com/AOSiP`
 const GERRIT_URL = 'https://review.aosip.dev/q'
+const DEVICES_URL = 'https://github.com/AOSiP-Devices'
 
 export async function handleRequest(request: Request): Promise<Response> {
   const relURL = request.url.replace(BASE_URL, '')
@@ -29,6 +29,23 @@ export async function handleRequest(request: Request): Promise<Response> {
         return Response.redirect(GERRIT_URL)
       case 2:
         return Response.redirect(`${GERRIT_URL}/${URLparts[1]}`)
+    }
+  } else if (URLparts[0] === 'd') {
+    switch (URLparts.length) {
+      case 1:
+        return Response.redirect(`${DEVICES_URL}`, 301)
+      case 2:
+        return Response.redirect(
+          `${DEVICES_URL}/${URLparts[1]}`,
+          301,
+        )
+      case 3:
+        return Response.redirect(
+          `${GITHUB_URL}/${URLparts[1]}/commit/${URLparts[2]}`,
+          301,
+        )
+      default:
+        break
     }
   }
   return fetch(request)
