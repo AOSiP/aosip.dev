654346be009405bcfea950689bcf648d8ff45b44
Akhil Narang <me@akhilnarang.dev>
2020-06-04 13:22:14 +0530

    redirekt: add route for jenkins
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- src/handler.ts
+++ src/handler.ts
@@ -2,6 +2,7 @@ const BASE_URL = 'https://aosip.dev/'
 const GITHUB_URL = `https://github.com/AOSiP`
 const GERRIT_URL = 'https://review.aosip.dev'
 const DEVICES_URL = 'https://github.com/AOSiP-Devices'
+const JENKINS_URL = 'https://jenkins.aosip.dev'
 
 export async function handleRequest(request: Request): Promise<Response> {
   const relURL = request.url.replace(BASE_URL, '')
@@ -42,6 +43,13 @@ export async function handleRequest(request: Request): Promise<Response> {
           `${GITHUB_URL}/${URLparts[1]}/commit/${URLparts[2]}`,
           301,
         )
+    }
+  } else if (URLparts[0] === 'j') {
+    switch (URLparts.length) {
+      case 1:
+        return Response.redirect(JENKINS_URL, 301)
+      case 2:
+        return Response.redirect(`${JENKINS_URL}/${URLparts[1]}`)
     }
   }
   return fetch(request)
